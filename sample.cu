02296f8d116b45d022c9289999b2f397ef380881
shyam2672 <91652089+shyam2672@users.noreply.github.com>
2022-06-21 21:31:29 +0530

    Create sample.cu

---
+++ sample.cu
@@ -0,0 +1,16 @@
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+struct ap{
+    int a;
+    int c[3];
+    double ch[32];
+    };
+      }
+      
